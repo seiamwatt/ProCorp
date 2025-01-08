a7b86b93f8d7b899b2ff10ff851228be882fa7a2
Vector <eiamw001@umn.edu>
2025-01-08 17:45:47 +0700

    feat : add item UI state kt file

---
+++ ProCorpV1/app/src/main/java/com/example/procorpv1/ui/theme/item/ItemEntryViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.procorpv1.ui.theme.item
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import  com.example.procorpv1.Data.Item
+import com.example.procorpv1.Data.ItemsRepository
+import java.text.NumberFormat
+
+
+class ItemEntryViewModel(private val itemsRepository: ItemsRepository) : ViewModel() {
+
+    
+}
